df79ad919d1f83e3f90bff60796fcbd4563b7c14
Bhavika Pahuja <bhavikapahuja96@gmail.com>
2020-08-16 21:55:35 +0530

    initial commit

---
+++ prog1.py
@@ -0,0 +1,12 @@
+a=int(input("Enter no1: "))
+b=int(input("Enter no2: "))
+
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+
+print("addition is:",add)
+print("subtraction is:",sub)
+print("multiplication is:",mul)
+print("division is:",div)
